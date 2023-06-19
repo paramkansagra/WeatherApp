cd2de0cd7b0d7615ecd02859f45644da3e75c8a8
Param Kansagra <paramjayesh.kansagra2021@vitstudent.ac.in>
2023-06-20 00:02:59 +0530

    Converted settings screen to scrollable one

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -22,113 +22,115 @@ class _SettingsScreenState extends State<SettingsScreen> {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "SETTINGS",
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w300,
-                    fontFamily: "DotMatrix",
-                  ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              "UNITS",
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    fontWeight: FontWeight.w300,
-                  ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            RadioListTile(
-              value: true,
-              groupValue: widget.celcius,
-              controlAffinity: ListTileControlAffinity.trailing,
-              contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              activeColor: Theme.of(context).colorScheme.onBackground,
-              onChanged: (value) {
-                widget.celcius = true;
-                setState(() {});
-              },
-              title: Text(
-                "Celcius",
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "SETTINGS",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: "DotMatrix",
+                    ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "UNITS",
                 style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       fontWeight: FontWeight.w300,
                     ),
               ),
-            ),
-            RadioListTile(
-              value: false,
-              groupValue: widget.celcius,
-              controlAffinity: ListTileControlAffinity.trailing,
-              contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              activeColor: Theme.of(context).colorScheme.onBackground,
-              onChanged: (value) {
-                widget.celcius = false;
-                setState(() {});
-              },
-              title: Text(
-                "Fahrenhite",
+              SizedBox(
+                height: 10,
+              ),
+              RadioListTile(
+                value: true,
+                groupValue: widget.celcius,
+                controlAffinity: ListTileControlAffinity.trailing,
+                contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                activeColor: Theme.of(context).colorScheme.onBackground,
+                onChanged: (value) {
+                  widget.celcius = true;
+                  setState(() {});
+                },
+                title: Text(
+                  "Celcius",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w300,
+                      ),
+                ),
+              ),
+              RadioListTile(
+                value: false,
+                groupValue: widget.celcius,
+                controlAffinity: ListTileControlAffinity.trailing,
+                contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                activeColor: Theme.of(context).colorScheme.onBackground,
+                onChanged: (value) {
+                  widget.celcius = false;
+                  setState(() {});
+                },
+                title: Text(
+                  "Fahrenhite",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w300,
+                      ),
+                ),
+              ),
+              ScreenDevider(
+                color:
+                    Theme.of(context).colorScheme.onBackground.withOpacity(0.3),
+              ),
+              Text(
+                "WIND SPEED",
                 style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       fontWeight: FontWeight.w300,
                     ),
               ),
-            ),
-            ScreenDevider(
-              color:
-                  Theme.of(context).colorScheme.onBackground.withOpacity(0.3),
-            ),
-            Text(
-              "WIND SPEED",
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    fontWeight: FontWeight.w300,
-                  ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            RadioListTile(
-              value: true,
-              groupValue: widget.wind,
-              controlAffinity: ListTileControlAffinity.trailing,
-              contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              activeColor: Theme.of(context).colorScheme.onBackground,
-              onChanged: (value) {
-                widget.wind = true;
-                setState(() {});
-              },
-              title: Text(
-                "KM/H",
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      fontWeight: FontWeight.w300,
-                    ),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            RadioListTile(
-              value: false,
-              groupValue: widget.wind,
-              controlAffinity: ListTileControlAffinity.trailing,
-              contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              activeColor: Theme.of(context).colorScheme.onBackground,
-              onChanged: (value) {
-                widget.wind = false;
-                setState(() {});
-              },
-              title: Text(
-                "MPH",
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      fontWeight: FontWeight.w300,
-                    ),
+              RadioListTile(
+                value: true,
+                groupValue: widget.wind,
+                controlAffinity: ListTileControlAffinity.trailing,
+                contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                activeColor: Theme.of(context).colorScheme.onBackground,
+                onChanged: (value) {
+                  widget.wind = true;
+                  setState(() {});
+                },
+                title: Text(
+                  "KM/H",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w300,
+                      ),
+                ),
               ),
-            ),
-          ],
+              RadioListTile(
+                value: false,
+                groupValue: widget.wind,
+                controlAffinity: ListTileControlAffinity.trailing,
+                contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                activeColor: Theme.of(context).colorScheme.onBackground,
+                onChanged: (value) {
+                  widget.wind = false;
+                  setState(() {});
+                },
+                title: Text(
+                  "MPH",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w300,
+                      ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
